5bec8d07372f359894f88ae1afb299b4a42f804d
Pat <56199797+perayepat@users.noreply.github.com>
2025-07-30 20:43:48 +0200

    basic convention plugins
    convention plugin has been created for :
    - compile options
    - defualtConfig
    - KotlinOptions

---
+++ build-logic/convention/build.gradle.kts
@@ -0,0 +1,25 @@
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+
+    `kotlin-dsl`
+}
+
+group = "com.example.runique.buildlogic"
+
+dependencies {
+    compileOnly(libs.android.gradlePlugin)
+    compileOnly(libs.android.tools.common)
+    compileOnly(libs.kotlin.gradlePlugin)
+    compileOnly(libs.ksp.gradlePlugin)
+    compileOnly(libs.room.gradlePlugin)
+}
+
+//Define all the convention plugins that we have
+gradlePlugin {
+    plugins {
+        register("androidApplication") {
+            id = "runique.android.application"
+            implementationClass = "AndroidApplicationConventionPlugin"
+        }
+    }
+}
--- build-logic/settings.gradle.kts
+++ build-logic/settings.gradle.kts
@@ -1,14 +1,13 @@
-pluginManagement {
+dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
     }
-}
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
+    versionCatalogs {
+        create("libs"){
+            from(files("../gradle/libs.versions.toml"))
+        }
     }
 }
 rootProject.name = "build-logic"
+include(":convention")
