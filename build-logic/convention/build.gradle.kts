7cf6c14c5026f13827086ba03cce28c3cb281a2f
Pat <56199797+perayepat@users.noreply.github.com>
2025-08-04 20:52:52 +0200

    Feature ui convention plugin
    Inject ViewModels easily via Koin.
    Automatically apply Compose setup to all feature UI modules.
    Include shared UI dependencies (design system, core UI).
    Keep modules clean, consistent, and scalable.
    Bundle dependency logic in a central place using version catalogs.

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -36,5 +36,10 @@ gradlePlugin {
             id = "runique.android.library.compose"
             implementationClass = "AndroidLibraryComposeConventionPlugin"
         }
+
+        register("androidFeatureUi"){
+            id = "runique.android.feature.ui"
+            implementationClass = "AndroidFeatureUiConventionPlugin"
+        }
     }
 }
