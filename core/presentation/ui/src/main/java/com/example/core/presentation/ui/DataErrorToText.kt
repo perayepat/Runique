3b087813b8bd8ddcbaa534106ff6505b455b2171
Pat <56199797+perayepat@users.noreply.github.com>
2025-08-05 02:46:13 +0200

    Creating a channel to get updates for onetime events

--- core/presentation/ui/src/main/java/com/example/core/presentation/ui/DataErrorToText.kt
+++ core/presentation/ui/src/main/java/com/example/core/presentation/ui/DataErrorToText.kt
@@ -23,13 +23,16 @@ fun DataError.asUiText(): UiText {
         DataError.Network.PAYLOAD_TOO_LARGE -> UiText.StringResource(
             R.string.error_payload_too_large
         )
+
         DataError.Network.SERVER_ERROR -> UiText.StringResource(
             R.string.error_server_error
         )
+
         DataError.Network.SERIALIZATION -> UiText.StringResource(
             R.string.error_serialization
         )
-        else  -> UiText.StringResource(
+
+        else -> UiText.StringResource(
             R.string.error_unknown
         )
 
